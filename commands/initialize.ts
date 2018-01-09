cb4e1b4f3be1c70c7bd63ddd170537ae54229b8a
eugene_ant <eugene.antonevich@gmail.com>
2018-01-10 01:57:22 +0300

    init project

---
+++ commands/initialize.ts
@@ -0,0 +1,9 @@
+import { Command } from '../engine';
+
+export class Init extends Command {
+    
+    run() {
+        throw new Error("Method not implemented.");
+    }
+
+}
---
+++ engine/Command.ts
@@ -0,0 +1,4 @@
+
+export abstract class Command {
+    abstract run(): any;
+}
