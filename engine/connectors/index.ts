3ea3aecc477d8a686b9569f48dc9683aaf4079bf
eugeneantonevich <eugene.antonevich@gmail.com>
2018-01-29 09:47:22 +0300

    .

--- engine/connectors/index.ts
+++ engine/connectors/index.ts
@@ -1,12 +1,13 @@
 import { ICliConnector, ICloudConnector } from "../../interfaces";
 import { ServerConnector } from "./serverConnector";
+import { AwsConnector } from "./awsConnector";
 
 export function getCliConnector(): ICliConnector {
     return new ServerConnector();
 }
 
 export function getCloudConnector(): ICloudConnector {
-    return new; 
+    return new AwsConnector();
 }
 
 
