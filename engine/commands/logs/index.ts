defd9847caac6fcd7763753f0023674b288aa21e
Eugene Antonevich <eugene.antonevich@8base.com>
2018-09-17 17:00:45 +0300

    add tail to logs

--- engine/commands/logs/index.ts
+++ engine/commands/logs/index.ts
@@ -4,9 +4,53 @@ import _ = require("lodash");
 import { GraphqlActions } from "../../../consts/GraphqlActions";
 import { translations } from "../../../common/translations";
 
+const sleep = (ms: number): Promise<void> => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+let lastMessage: string = "";
+
+const filterMessage = (messages: string[]): string[] => {
+  const index = messages.indexOf(lastMessage);
+
+  if (index === -1) {
+    lastMessage = messages.length > 0 ? _.last(messages) : "";
+    return messages;
+  }
+
+  if (index + 1 === messages.length) {
+    return [];
+  }
+
+  lastMessage = _.last(messages);
+  return _.slice(messages, index);
+};
+
+const readLogs = async (functionName: string, context: Context) => {
+
+  while(true) {
+    const MS_PER_MINUTE = 60000;
+    const minutes = 3;
+    const start = new Date(Date.now() - minutes * MS_PER_MINUTE);
+
+    await sleep(1000);
+
+    const result = await context.request(GraphqlActions.logs, { functionName, startTime: start.toISOString() });
+    const logs = filterMessage(result.logs);
+    if (logs.length > 0) {
+      context.logger.info(logs);
+    }
+  }
+
+};
+
 export default {
   name: "logs",
   handler: async (params: any, context: Context) => {
+    if (params["t"] ) {
+      return await readLogs(params._[1], context);
+    }
+
     context.spinner.start(context.i18n.t("logs_in_progress"));
 
     const result = await context.request(GraphqlActions.logs, { functionName: params._[1], limit: params.n });
@@ -24,6 +68,11 @@ export default {
         describe: "number of lines to display (default: 10, max: 100)",
         type: "number",
         coerce: arg => arg > 100 ? 100 : arg
+      })
+      .option("t", {
+        alias: 'tail',
+        describe: "continually stream logs",
+        type: "boolean"
       });
   }
 };
