0dfa3ac7102963ee2495407942f03068116c37cb
eugene.antonevich <eugene.antonevich@8base.com>
2020-05-29 14:51:56 +0300

    deploy -> commit

--- src/engine/commands/migration/commands/commit.ts
+++ src/engine/commands/migration/commands/commit.ts
@@ -1,13 +1,17 @@
 import { Context } from '../../../../common/context';
 import { translations } from '../../../../common/translations';
-import { executeAsync } from "../../../../common/execute";
+import { executeAsync, executeDeploy } from "../../../../common/execute";
 import { GraphqlAsyncActions } from "../../../../consts/GraphqlActions";
 import { ProjectConfigurationState } from "../../../../common/configuraion";
+import { DeployModeType } from "../../../../interfaces/Extensions";
 
 export default {
   command: 'commit',
   handler: async (params: any, context: Context) => {
-    ProjectConfigurationState.expectConfigured(context);
+    ProjectConfigurationState.expectHasProject(context);
+    context.initializeProject();
+    await executeDeploy(context, { mode: DeployModeType.migrations });
+
     context.spinner.start(context.i18n.t('migration_commit_in_progress'));
     await executeAsync(context, GraphqlAsyncActions.commit, { environmentId: context.workspaceConfig.environment.id })
   },
