4b2efbc00f562559b482f766db830eebb3d87dc8
Osipov Vladimir <zouxuoz@gmail.com>
2020-08-03 13:36:15 +0300

    chore: typings for the functions

---
+++ src/types.ts
@@ -0,0 +1,37 @@
+import { DocumentNode } from 'graphql';
+
+export type GqlRequest = <Result = {}, Variables = {}>(
+  gqlTag: DocumentNode,
+  variables?: Variables,
+  options?: {
+    checkPermissions?: boolean;
+    headers?: Record<string, any>;
+  },
+) => Promise<Result>;
+
+export type FunctionContext = {
+  api: {
+    gqlRequest: GqlRequest;
+  };
+  invokeFunction: <Result = {}, Args = {}>(
+    name: string,
+    args?: Args,
+    options?: { waitForResponse: boolean },
+  ) => Promise<Result>;
+};
+
+export type FunctionEvent<Data = {}, OriginalObject = {}, ExtendObject = {}, Error = {}> = {
+  data: Data;
+  originalObject: OriginalObject;
+  errors: Error[];
+  body?: string;
+} & ExtendObject;
+
+export type FunctionResult<Data = {}, OriginalObject = {}, ExtendObject = {}, Error = {}> = Promise<
+  {
+    data?: Data;
+    originalObject?: OriginalObject;
+    errors?: Error[];
+    body?: string;
+  } & ExtendObject
+>;
