50f7da43e042c9bc49467ff1d7bb6c91e2a77b70
Oscar Corcho <85709565+dav1nx2@users.noreply.github.com>
2023-09-12 10:21:07 -0500

    updating dependencies

--- src/engine/commands/init/index.ts
+++ src/engine/commands/init/index.ts
@@ -140,8 +140,6 @@ export default {
         throw new Error(context.i18n.t('workspace_with_id_doesnt_exist', { id: workspaceId }));
       }
 
-      console.log('workspace information', workspace);
-
       host = workspace.apiHost;
     }
 
